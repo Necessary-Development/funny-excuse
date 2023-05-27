4c9ee9030592eaa13f5563d930fa727e3d0f2d00
Matt Megarry <mattmegarry@gmail.com>
2023-05-27 20:32:40 +0100

    hacky: server can make request to openai

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, jsonify, request, url_for
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -9,6 +12,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    """  
     if request.method == "POST":
         animal = request.form["animal"]
         response = openai.Completion.create(
@@ -17,9 +21,15 @@ def index():
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    """
+    openai_response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt("fish"),
+            temperature=0.6,
+        )
+    # result = request.args.get("result")
+    result = {"hello": "world"}
+    return jsonify(openai_response)
 
 
 def generate_prompt(animal):
