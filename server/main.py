f38d45fa843546182c4905c67b23214d7f618ad7
Matt Megarry <mattmegarry@gmail.com>
2023-06-05 17:09:03 +0100

    delete run command

--- server/main.py
+++ server/main.py
@@ -50,6 +50,3 @@ def generate_prompt(valence_clause, adjective, plural_noun, decision):
     """.format(
         valence_clause, adjective, plural_noun, decision,
     )
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
